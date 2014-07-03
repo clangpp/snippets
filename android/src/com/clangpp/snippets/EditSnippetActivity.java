b7b747552a661f35a96b22792ebd67765e805a2f
clangpp <clangpp@gmail.com>
2014-07-03 17:22:44 +0800

    Adds async retrieving Snippet to EditSnippetActivity.

--- android/src/com/clangpp/snippets/EditSnippetActivity.java
+++ android/src/com/clangpp/snippets/EditSnippetActivity.java
@@ -5,18 +5,39 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class EditSnippetActivity extends Activity {
+  class RetrieveSnippetAsyncTask extends AsyncTask<String, Void, Snippet> {
+    SnippetService snippetService;
+
+    public RetrieveSnippetAsyncTask(SnippetService snippetService) {
+      this.snippetService = snippetService;
+    }
+
+    @Override
+    protected Snippet doInBackground(String... params) {
+      return snippetService.getSnippet(params[0]);
+    }
+
+    @Override
+    protected void onPostExecute(Snippet result) {
+      snippet = result;
+      showSnippet();
+    }
+  }
+
   private static final String TAG = EditSnippetActivity.class.getName();
   private static final String TIMESTAMP_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS ZZZZZ";
 
   private EditText snippetContent;
   private TextView snippetTimestamp;
   private Snippet snippet;
+  private SnippetService snippetService;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +47,13 @@ public class EditSnippetActivity extends Activity {
     snippetContent = (EditText) findViewById(R.id.snippet_content);
     snippetTimestamp = (TextView) findViewById(R.id.snippet_timestamp);
 
+    // TODO(clangpp): Add option to read snippet ID from intent.
     snippet = Snippet.newBuilder().build();
+
+    snippetService = SnippetServiceFactory.getSingletonCachedSnippetService();
+    if (!snippet.getId().equals(Snippet.Builder.EMPTY_ID)) {
+      new RetrieveSnippetAsyncTask(snippetService).execute(snippet.getId());
+    }
     showSnippet();
   }
 
@@ -51,7 +78,7 @@ public class EditSnippetActivity extends Activity {
 
   void saveSnippet() {
     Snippet.Builder snippetBuilder = Snippet.newBuilder(snippet);
-    
+
     snippetBuilder.setContent(snippetContent.getText().toString());
 
     SimpleDateFormat sdf = new SimpleDateFormat(TIMESTAMP_FORMAT);
@@ -62,7 +89,7 @@ public class EditSnippetActivity extends Activity {
     } catch (ParseException e) {
       Log.e(TAG, "Parse failed", e);
     }
-    
+
     snippet = snippetBuilder.build();
   }
 }
